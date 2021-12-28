0ed525cdcf21ab25ba217563b5edb90e112257b1
Krisna <krisnaaaputraaa@gmail.com>
2021-12-28 20:17:09 +0700

    nambahin tilt.js pada section pproject

--- js/script.js
+++ js/script.js
@@ -1,3 +1,13 @@
 AOS.init({
   once: true
 });
+
+VanillaTilt.init(document.querySelectorAll(".kartu"), {
+  max: 15,
+  speed: 400,
+  glare: true,
+  "max-glare": .5,
+  easing: "cubic-bezier(.03,.98,.52,.99)",
+  gyroscope: true,
+  scale: 1.1,
+});
